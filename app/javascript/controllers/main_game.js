0764dcb6e70745a9defa047f6491c6de9839a7ee
Ocha-Misaki <himawari.158fuji@gmail.com>
2024-02-02 14:06:16 +0900

    jsファイルの作成

--- app/javascript/controllers/main_game.js
+++ app/javascript/controllers/main_game.js
@@ -1,56 +1,126 @@
-const container = document.querySelector(".container")
-const bloodSpot = document.querySelector(".beans")
-const startBtn = document.querySelector(".startBtn")
-const cursor = document.querySelector(".cursor")
-const enemy = document.getElementById("enemy")
-const enemySize = parseInt(
-  window.getComputedStyle(enemy).getPropertyValue("height")
-)
-const contHeight = container.offsetHeight
-const contWidth = container.offsetWidth
+const body = document.querySelector("body")
+const container = document.getElementsByClassName("container")[1]
+const contHeight = (container.style.height = 300)
+const contWidth = (container.style.width = 300)
+
+const pElement = document.createElement("p")
+pElement.textContent = `残り20秒`
+const scorePElement = document.createElement("p")
+scorePElement.textContent = `スコア: `
+const buttonElement = document.createElement("button")
+buttonElement.classList.add("btn")
+buttonElement.classList.add("btn-primary")
+buttonElement.textContent = "ゲームスタート"
+body.appendChild(scorePElement)
+body.appendChild(pElement)
+body.appendChild(buttonElement)
+
+const enemyImg = document.createElement("img")
+enemyImg.src = "/assets/setsubun_kurooni.png"
+enemyImg.style.position = "absolute"
+enemyImg.style.height = "200px"
+enemyImg.style.width = "200px"
+
+const pigeonImg = document.createElement("img")
+pigeonImg.src = "/assets/hato.png"
+pigeonImg.style.position = "absolute"
+pigeonImg.style.height = "200px"
+pigeonImg.style.width = "200px"
+
+const imgHeight = 20
+const imgWidth = 20
+const imgArray = [enemyImg, pigeonImg]
+
+const enemyAudio = document.createElement("audio")
+enemyAudio.src = "/assets/Quiz-Correct_Answer02-1.mp3"
+
+const buzzerAudio = document.createElement("audio")
+buzzerAudio.src = "/assets/Buzzer02-2.mp3"
 
 let score = 0
-const winScore = 10
-let displayTimer = document.getElementById("timer")
-let timer = 20
 
-function showTimer() {
-  timer--
-  displayTimer.innerText = `残り時間: ${timer}`
-  if (timer === 0) {
-    alert("ゲームオーバー")
-    location.reload()
-  }
+const startTimer = (seconds, gameIntervalId) => {
+  const startTime = Date.now()
+  const endTime = startTime + seconds * 1000
+  let intervalId = setInterval(() => {
+    const currentTime = Date.now()
+    const restTime = Math.ceil((endTime - currentTime) / 1000)
+    pElement.textContent = `残り${restTime}秒`
+    if (restTime < 1) {
+      clearInterval(intervalId)
+      clearInterval(gameIntervalId)
+      alert(`あなたのスコアは${score}でした！`)
+      sendScore(score)
+      location.reload()
+    }
+  }, 1000)
 }
 
-window.addEventListener("mousemove", (e) => {
-  cursor.style.top = e.pageY + "px"
-  cursor.style.left = e.pageX + "px"
-})
+const clickEnemy = () => {
+  window.addEventListener("click", (e) => {
+    if (e.target === enemyImg) {
+      enemyAudio.currentTime = 0
+      enemyAudio.play()
+      score += 10
+    } else if (e.target === pigeonImg) {
+      buzzerAudio.currentTime = 0
+      buzzerAudio.play()
+      score -= 10
+    }
+    scorePElement.textContent = `スコア: ${score}`
+  })
+}
 
-window.addEventListener("click", (e) => {
-  bloodSpot.style.top = e.pageY + "px"
-  bloodSpot.style.left = e.pageX + "px"
+const shuffleImg = () => {
+  let randNum = Math.floor(Math.random() * 2)
+  return imgArray[randNum]
+}
 
-  if (e.target === enemy) score++
-  startBtn.innerText = "SCORE: " + score
-  if (score === winScore) {
-    alert("ゲームクリア")
-    location.reload()
+const setGame = () => {
+  let gameIntervalId = setInterval(() => {
+    container.appendChild(shuffleImg())
+    let randTop = Math.random() * (contHeight - imgHeight)
+    let randWidth = Math.random() * (contWidth - imgWidth)
+    shuffleImg().style.top = randTop + "px"
+    shuffleImg().style.left = randWidth + "px"
+  }, 500)
+  clickEnemy(1000)
+  startTimer(10, gameIntervalId)
+}
+
+const sendScore = (score) => {
+  fetch("/users/:user_id/scores", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "X-CSRF-Token": getCsrfToken(),
+    },
+    body: JSON.stringify({result: score}),
+  })
+    .then((response) => {
+      if (response.ok) {
+        console.log("スコアが正常に送信されました")
+      } else {
+        console.error("スコアの送信中にエラーが発生しました")
+      }
+    })
+    .catch((error) => {
+      console.error("通信エラー:", error)
+    })
+}
+
+const getCsrfToken = () => {
+  const metas = document.getElementsByTagName("meta")
+  for (let meta of metas) {
+    if (meta.getAttribute("name") === "csrf-token") {
+      console.log("csrf-token:", meta.getAttribute("content"))
+      return meta.getAttribute("content")
+    }
   }
+  return ""
+}
+
+buttonElement.addEventListener("click", () => {
+  buttonElement.disabled = true
+  setGame()
 })
-
-startBtn.addEventListener("click", () => {
-  startBtn.disabled = "true"
-  enemy.style.display = "block"
-
-  startBtn.innerText = "SCORE: " + score
-  setInterval(showTimer, 1000)
-})
-
-setInterval(() => {
-  const randTop = Math.random() * (contHeight - enemySize)
-  const randLeft = Math.random() * (contWidth - enemySize)
-  enemy.style.top = randTop + "px"
-  enemy.style.left = randLeft + "px"
-}, 700)
